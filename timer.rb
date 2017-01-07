909d6d46aae0bba8f0306f39bc4a79325a5da52c
Daniel Colson <danieljamescolson@gmail.com>
2017-01-07 01:43:43 -0500

    Build a little Ruby Timer

---
+++ timer.rb
@@ -0,0 +1,49 @@
+class Timer
+  @intervals = [];
+
+  def self.set_timeout(callback, seconds)
+    end_time = Time.now + seconds
+
+    Thread.new do
+      while Time.now <= end_time
+        Thread.pass
+      end
+
+      callback.()
+    end
+  end
+
+  def self.set_interval(callback, seconds)
+    interval = self.new(callback, seconds)
+    interval.set!
+    @intervals << interval
+    @intervals.length - 1
+  end
+
+  def self.clear_interval(index)
+    @intervals[index].clear!
+  end
+
+  def self.wait_for_intervals
+    Thread.list.drop(1).each(&:join)
+  end
+
+  def initialize(callback, seconds)
+    @callback, @seconds, @active = callback, seconds, true
+  end
+
+  def set!
+    Thread.new do
+      while active
+        Timer.set_timeout(callback, seconds).join
+      end
+    end
+  end
+
+  def clear!
+    @active = false
+  end
+
+  private
+  attr_reader :callback, :seconds, :active
+end
